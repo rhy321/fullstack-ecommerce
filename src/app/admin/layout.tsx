c02c8351ba1fd32daeefaafb1168c5cf2c7b5cd7
rhy321 <rhyahm25@gmail.com>
2026-08-26 22:39:29 +0530

    created admin page

---
+++ src/app/admin/layout.tsx
@@ -0,0 +1,15 @@
+import { Nav, NavLink } from "@/components/Nav";
+
+export default function AdminLayout({ children }: LayoutProps<"/">) {
+  return (
+    <>
+    <Nav>
+      <NavLink href="/admin">Dashboard</NavLink>
+      <NavLink href="/admin/products">Products</NavLink>
+      <NavLink href="/admin/users">Customers</NavLink>
+      <NavLink href="/admin/orders">Sales</NavLink>
+    </Nav>
+    <div className="container my-6">{children}</div>
+    </>
+  )
+}
