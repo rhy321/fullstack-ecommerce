3d0887bad52c58357df0e7ec1e961733235ae36f
rhy321 <rhyahm25@gmail.com>
2026-09-03 21:57:49 +0530

    product and products/new page and form

---
+++ src/app/admin/products/_components/ProductForm.tsx
@@ -0,0 +1,55 @@
+"use client"
+
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import { Textarea } from "@/components/ui/textarea"
+import { formatCurrency } from "@/lib/formatters"
+import { useState } from "react"
+import { addProduct } from "../../_actions/product"
+
+export function ProductForm() {
+  const [priceInCents, setPriceInCents] = useState<number | "">("")
+  return (
+
+    <form action={addProduct} className="space-y-8">
+
+      <div className="space-y-2">
+        <Label htmlFor="name">Name</Label>
+        <Input type="text" id="name" name="name" required></Input>
+      </div>
+
+      <div className="space-y-2">
+        <Label htmlFor="priceInCents">Price In Cents</Label>
+        <Input
+          type="number"
+          id="priceInCents"
+          name="priceInCents"
+          value={priceInCents}
+          onChange={e => setPriceInCents(Number(e.target.value))}
+          required>
+        </Input>
+        <div className="text-muted-foreground">
+          {formatCurrency((priceInCents || 0) / 100)}
+        </div>
+      </div>
+
+      <div className="space-y-2">
+        <Label htmlFor="description">Description</Label>
+        <Textarea id="description" name="description" required></Textarea>
+      </div>
+
+      <div className="space-y-2">
+        <Label htmlFor="file">File</Label>
+        <Input type="file" id="file" name="file" required></Input>
+      </div>
+
+      <div className="space-y-2">
+        <Label htmlFor="image">Image</Label>
+        <Input type="file" id="image" name="image" required></Input>
+      </div>
+
+      <Button type="submit">Save</Button>
+    </form>
+  )
+}
---
+++ src/app/admin/users/page.tsx
@@ -0,0 +1,5 @@
+export default function AdminUsersPage() {
+  return (
+    <h1>gg</h1>
+  )
+}
