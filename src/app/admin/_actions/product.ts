5e93edd067b6102a7e664fb87940b1274aa302f0
rhy321 <rhyahm25@gmail.com>
2026-09-05 20:03:24 +0530

    error messages for product creation and pending state for submit

--- src/app/admin/_actions/product.ts
+++ src/app/admin/_actions/product.ts
@@ -19,11 +19,35 @@ const addSchema = z.object({
   image: imageSchema.refine(file => file.size > 0, "Required")
 })
 
-export async function addProduct(formData: FormData) {
+type FormState = {
+  errors?: Record<string, string[] | undefined>
+  values?: {
+    name: string
+    description: string
+    priceInCents: string
+  }
+}
+
+//: Promise<FormState> means "This function will eventually return a FormState object."
+
+export async function addProduct(
+  _prevState: FormState,
+  formData: FormData
+): Promise<FormState> {
+  
+  const values = {
+    name: String(formData.get("name") ?? ""),
+    description: String(formData.get("description") ?? ""),
+    priceInCents: String(formData.get("priceInCents") ?? ""),
+  }
+
   const result = addSchema.safeParse(Object.fromEntries(formData.entries()))
 
-  if (result.success === false) {
-    return console.error();
+  if (!result.success) {
+    return {
+      errors: z.flattenError(result.error).fieldErrors,
+      values,
+    }
   }
 
   const data = result.data
@@ -41,6 +65,7 @@ export async function addProduct(formData: FormData) {
   await fs.writeFile(`public/${imagePath}`, Buffer.from(await data.image.arrayBuffer()))
 
   await db.product.create({ data: {
+    isAvailableForPurchase: false,
     name: data.name,
     description: data.description,
     priceInCents: data.priceInCents,
--- src/app/admin/products/_components/ProductForm.tsx
+++ src/app/admin/products/_components/ProductForm.tsx
@@ -5,18 +5,43 @@ import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 import { Textarea } from "@/components/ui/textarea"
 import { formatCurrency } from "@/lib/formatters"
-import { useState } from "react"
+import { startTransition, useActionState, useEffect, useState } from "react"
 import { addProduct } from "../../_actions/product"
+import { useFormStatus } from "react-dom"
 
 export function ProductForm() {
+  const [state, action] = useActionState(addProduct, {})
+  const [name, setName] = useState("")
+  const [description, setDescription] = useState("")
   const [priceInCents, setPriceInCents] = useState<number | "">("")
+
+  useEffect(() => {
+    if (!state.values) return
+
+    startTransition(() => {
+      setName(state.values!.name)
+      setDescription(state.values!.description)
+      setPriceInCents(state.values!.priceInCents === "" ? "" : Number(state.values!.priceInCents))
+    })
+  }, [state.values])
+
   return (
 
-    <form action={addProduct} className="space-y-8">
+    <form action={action} className="space-y-8">
 
       <div className="space-y-2">
         <Label htmlFor="name">Name</Label>
-        <Input type="text" id="name" name="name" required></Input>
+        <Input
+          type="text"
+          id="name"
+          name="name"
+          value={name}
+          onChange={e => setName(e.target.value)}
+          required
+        />
+        {state.errors?.name?.[0] && (
+          <div className="text-destructive">{state.errors.name[0]}</div>
+        )}
       </div>
 
       <div className="space-y-2">
@@ -26,30 +51,58 @@ export function ProductForm() {
           id="priceInCents"
           name="priceInCents"
           value={priceInCents}
-          onChange={e => setPriceInCents(Number(e.target.value))}
-          required>
-        </Input>
+          onChange={e => setPriceInCents(e.target.value === "" ? "" : Number(e.target.value))}
+          required
+        />
         <div className="text-muted-foreground">
           {formatCurrency((priceInCents || 0) / 100)}
         </div>
+        {state.errors?.priceInCents?.[0] && (
+          <div className="text-destructive">{state.errors.priceInCents[0]}</div>
+        )}
       </div>
 
       <div className="space-y-2">
         <Label htmlFor="description">Description</Label>
-        <Textarea id="description" name="description" required></Textarea>
+        <Textarea
+          id="description"
+          name="description"
+          value={description}
+          onChange={e => setDescription(e.target.value)}
+          required
+        />
+        {state.errors?.description?.[0] && (
+          <div className="text-destructive">{state.errors.description[0]}</div>
+        )}
       </div>
 
       <div className="space-y-2">
         <Label htmlFor="file">File</Label>
         <Input type="file" id="file" name="file" required></Input>
+        {state.errors?.file?.[0] && (
+          <div className="text-destructive">{state.errors.file[0]}</div>
+        )}
       </div>
 
       <div className="space-y-2">
         <Label htmlFor="image">Image</Label>
         <Input type="file" id="image" name="image" required></Input>
+        {state.errors?.image?.[0] && (
+          <div className="text-destructive">{state.errors.image[0]}</div>
+        )}
       </div>
 
-      <Button type="submit">Save</Button>
+      <SubmitButton />
     </form>
+  )
+}
+
+function SubmitButton() {
+  const { pending } = useFormStatus()
+
+  return (
+    <Button type="submit" disabled={pending}>
+      {pending ? "Saving..." : "Save"}
+    </Button>
   )
 }
